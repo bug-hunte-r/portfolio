68f3932fa3d187cd3ac2c6560032142b3bb99b39
shayan <shayanjafarnejad75@gmail.com>
2025-07-27 17:56:09 +0330

    change web color and added some animation

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 import style from './global.css'
+import Orb from '../anim/Orb';
 
 export default function Home() {
   return (
     <div className='container'>
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <Orb
+          hoverIntensity={0.5}
+          rotateOnHover={true}
+          hue={0}
+          forceHoverState={false}
+        />
+      </div>
     </div>
   );
 }
--- components/templates/Navbar.js
+++ components/templates/Navbar.js
@@ -1,17 +1,100 @@
+"use client"
 import React from 'react'
 import navbar from '../../styles/navbar/navbar.css'
 import mediaNavbar from '../../styles/navbar/mediaNavbar.css'
 import Link from 'next/link'
+import SplitText from './../../anim/SplitText'
+
 
 function Navbar() {
+
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
+
+
     return (
         <div className='container'>
-            <h1 className='logo'>JAF.</h1>
+            <SplitText
+                text="JAF."
+                className="text-2xl font-semibold text-center logo"
+                delay={400}
+                duration={2}
+                ease="power3.out"
+                splitType="chars"
+                from={{ opacity: 0, y: 40 }}
+                to={{ opacity: 1, y: 0 }}
+                threshold={0.1}
+                rootMargin="-100px"
+                textAlign="center"
+                onLetterAnimationComplete={handleAnimationComplete}
+            />
+
             <div className='container-links'>
-                <Link href={'/'} className='nav-links'>Home</Link>
-                <Link href={'/'} className='nav-links'>About</Link>
-                <Link href={'/'} className='nav-links'>Projects</Link>
-                <Link href={'/'} className='nav-links'>Contact</Link>
+                <Link href={'/'}>
+                    <SplitText
+                        text="Home"
+                        className="text-2xl font-semibold text-center nav-links"
+                        delay={200}
+                        duration={0.9}
+                        ease="power3.out"
+                        splitType="chars"
+                        from={{ opacity: 0, y: 40 }}
+                        to={{ opacity: 1, y: 0 }}
+                        threshold={0.1}
+                        rootMargin="-100px"
+                        textAlign="center"
+                        onLetterAnimationComplete={handleAnimationComplete}
+                    />
+                </Link>
+                <Link href={'/'}>
+                    <SplitText
+                        text="About"
+                        className="text-2xl font-semibold text-center nav-links"
+                        delay={200}
+                        duration={0.9}
+                        ease="power3.out"
+                        splitType="chars"
+                        from={{ opacity: 0, y: 40 }}
+                        to={{ opacity: 1, y: 0 }}
+                        threshold={0.1}
+                        rootMargin="-100px"
+                        textAlign="center"
+                        onLetterAnimationComplete={handleAnimationComplete}
+                    />
+                </Link>
+                <Link href={'/'}>
+                    <SplitText
+                        text="Projects"
+                        className="text-2xl font-semibold text-center nav-links"
+                        delay={200}
+                        duration={0.9}
+                        ease="power3.out"
+                        splitType="chars"
+                        from={{ opacity: 0, y: 40 }}
+                        to={{ opacity: 1, y: 0 }}
+                        threshold={0.1}
+                        rootMargin="-100px"
+                        textAlign="center"
+                        onLetterAnimationComplete={handleAnimationComplete}
+                    />
+                </Link>
+                <Link href={'/'}>
+                    <SplitText
+                        text="Contact"
+                        className="text-2xl font-semibold text-center nav-links"
+                        delay={200}
+                        duration={0.9}
+                        ease="power3.out"
+                        splitType="chars"
+                        from={{ opacity: 0, y: 40 }}
+                        to={{ opacity: 1, y: 0 }}
+                        threshold={0.1}
+                        rootMargin="-100px"
+                        textAlign="center"
+                        onLetterAnimationComplete={handleAnimationComplete}
+                    />
+                </Link>
             </div>
         </div>
     )
