7f922e20db7b391391c7114621a4337a9131c4a1
shayan <shayanjafarnejad75@gmail.com>
2025-07-27 14:14:33 +0330

    set bottomvar for mobile size

--- components/templates/Bottombar.js
+++ components/templates/Bottombar.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { FaHome } from "react-icons/fa";
-import { FaAddressBook } from "react-icons/fa";
-import { MdOutlineWeb } from "react-icons/md";
-import { FaSquarePhoneFlip } from "react-icons/fa6";
-
+import { IoHomeOutline } from "react-icons/io5";
+import { LuBookUser } from "react-icons/lu";
+import { CgWebsite } from "react-icons/cg";
+import { HiOutlinePhone } from "react-icons/hi";
+import bottombar from './../../styles/bottombar/bottombar.css'
+import mediaBottombar from './../../styles/bottombar/mediaBottombar.css'
 
 function Bottombar() {
     return (
         <div className='container'>
             <div className='container-bottombar'>
-                <FaHome className='icons-bottombar' />
-                <FaAddressBook className='icons-bottombar' />
-                <MdOutlineWeb className='icons-bottombar' />
-                <FaSquarePhoneFlip className='icons-bottombar' />
+                <IoHomeOutline className='icons-bottombar' size={23}/>
+                <LuBookUser className='icons-bottombar' size={23}/>
+                <CgWebsite className='icons-bottombar' size={23}/>
+                <HiOutlinePhone className='icons-bottombar' size={23} />
             </div>
         </div>
     )
