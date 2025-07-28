cd016f1bb608311a4fb5af99011d7f1c40d25f53
shayan <shayanjafarnejad75@gmail.com>
2025-07-28 12:59:51 +0330

    Develope about section

--- components/templates/About.js
+++ components/templates/About.js
@@ -6,8 +6,11 @@ import media from '../../styles/about/mediaAbout.css'
 function About() {
   return (
     <div className='container'>
-        <Image src={'/images/iran.png'} width={1000} height={1000} alt='iran' className='img-iran' />
-        <img src='images/process-line.png' className='img-procces' />
+      <Image src={'/images/iran.png'} width={1000} height={1000} alt='iran' className='img-iran' />
+      <img src='images/process-line.png' className='img-procces' />
+      <p className='my-city'>West Azerbaijan</p>
+      <div className='container-about'>
+      </div>
     </div>
   )
 }
