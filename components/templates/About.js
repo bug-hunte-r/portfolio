c3a2c679355f0e66d8e38995e3f65acab3318edd
shayan <shayanjafarnejad75@gmail.com>
2025-07-28 15:08:40 +0330

    make responsiv the about section

--- components/templates/About.js
+++ components/templates/About.js
@@ -2,26 +2,23 @@ import Image from 'next/image'
 import React from 'react'
 import about from '../../styles/about/about.css'
 import media from '../../styles/about/mediaAbout.css'
+import { FaCircle } from "react-icons/fa6";
 
 function About() {
   return (
     <div className='container'>
 
+      <h1 className='title-about'>About</h1>
+
       <Image src={'/images/iran.png'} width={1000} height={1000} alt='iran' className='img-iran' />
-      <img src='images/process-line.png' className='img-procces' />
-      <p className='my-city'>West Azerbaijan</p>
 
       <div className='container-about'>
         <h2 className='my-name-about'>I`m Shayan Jafarnejad</h2>
         <div className='container-texts'>
-          <Image src={'/images/circle.png'} width={100} height={100} alt='circle' className='circles-about' />
-          <p className='texts-about'>I was born in Iran-Kurdistan</p>
-          <Image src={'/images/circle.png'} width={100} height={100} alt='circle' className='circles-about' />
-          <p className='texts-about'>I`m 17 years old</p>
-          <Image src={'/images/circle.png'} width={100} height={100} alt='circle' className='circles-about' />
-          <p className='texts-about'>I started my serious activity +2 years ago.</p>
-          <Image src={'/images/circle.png'} width={100} height={100} alt='circle' className='circles-about' />
-          <p className='texts-about'>I am a front-end developer and have recently entered the back-end field.</p>
+          <p className='texts-about'><FaCircle className='circles-about' />I was born in Iran-Kurdistan</p>
+          <p className='texts-about'><FaCircle className='circles-about' />I`m 17 years old</p>
+          <p className='texts-about'><FaCircle className='circles-about' />I started my serious activity +2 years ago.</p>
+          <p className='texts-about'><FaCircle className='circles-about' />I`m a frontend developer and have recently entered the backend field</p>
         </div>
       </div>
 
