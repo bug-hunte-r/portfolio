140e09fd3995614ec0c0c6d329b2b07d3c4049f7
shayan <shayanjafarnejad75@gmail.com>
2025-08-02 12:18:02 +0330

    make responsive the project section

--- components/modules/CardProject.js
+++ components/modules/CardProject.js
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState } from 'react'
 import cardProject from '../../styles/cardProject/cardProject.css'
+import media from '../../styles/cardProject/mediaCardProject.css'
 import Image from 'next/image'
 import FadeContent from '@/anim/FadeContent'
 
@@ -28,16 +29,16 @@ function CardProject({ GiahLand, FitLand }) {
   return (
     <>
       <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-        <div className='card-project' onMouseEnter={blurHandlerGi} onMouseLeave={UnBlurHandlerGi}>
-          <Image src={GiahLand?.img} width={700} height={700} alt='img-project' className='img-project' />
-          <p className={`${isBlurGi ? 'desc-project-card' : 'desc-none'}`}>{GiahLand?.title} <br /> <br /> {GiahLand?.start}<br /> <br /> {GiahLand?.finish}</p>
+        <div className='card-project gi' onMouseEnter={blurHandlerGi} onMouseLeave={UnBlurHandlerGi}>
+          <Image src={GiahLand?.img} width={700} height={700} alt='img-project' className={`${isBlurGi ? 'img-project' : 'img-project-unBlur'}`} />
+          <p className={`${isBlurGi ? 'desc-project-card' : 'desc-project-card-none'}`}>{GiahLand?.title} <br /> <br /> {GiahLand?.start}<br /> <br /> {GiahLand?.finish}</p>
         </div>
       </FadeContent>
 
       <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
         <div className='card-project' onMouseEnter={blurHandler} onMouseLeave={UnBlurHandler}>
-          <Image src={FitLand?.img} width={700} height={700} alt='img-project' className='img-project' />
-          <p className={`${isBlur ? 'desc-project-card' : 'desc-none'}`}>{FitLand?.title} <br /> <br /> {FitLand?.start}<br /> <br /> {FitLand?.finish}</p>
+        <Image src={FitLand?.img} width={700} height={700} alt='img-project' className={`${isBlur ? 'img-project' : 'img-project-unBlur'}`} />
+        <p className={`${isBlur ? 'desc-project-card' : 'desc-project-card-none'}`}>{FitLand?.title} <br /> <br /> {FitLand?.start}<br /> <br /> {FitLand?.finish}</p>
         </div>
       </FadeContent>
 
