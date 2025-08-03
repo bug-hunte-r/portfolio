d22fbb605fbe7ff5a1cd09a20f4661e13fe4ce99
shayan <shayanjafarnejad75@gmail.com>
2025-08-03 18:58:08 +0330

    develop html templates for contact

--- components/templates/Contact.js
+++ components/templates/Contact.js
@@ -3,8 +3,14 @@ import media from '../../styles/contact/mediaContact.css'
 
 function Contact() {
   return (
-    <div>
-      
+    <div className='container'>
+      <h2 className='title-contact'>Contact</h2>
+      <div className='form-contact'>
+        <input placeholder='Name' className='inputs-contact' type='text' />
+        <input placeholder='Email' className='inputs-contact' type='email' />
+        <textarea placeholder='Your Message' className='inputs-contact' type='text' />
+        <button className='btn-contact'>Send</button>
+      </div>
     </div>
   )
 }
