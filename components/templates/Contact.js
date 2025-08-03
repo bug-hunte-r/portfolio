d2183b71f21fb3a6edd3ae487c46f463e2d160dd
shayan <shayanjafarnejad75@gmail.com>
2025-08-03 20:08:25 +0330

    set styles for contact

--- components/templates/Contact.js
+++ components/templates/Contact.js
@@ -1,15 +1,30 @@
 import contact from '../../styles/contact/contact.css'
 import media from '../../styles/contact/mediaContact.css'
+import DarkVeil from '@/anim/DarkVeil'
 
 function Contact() {
   return (
     <div className='container'>
-      <h2 className='title-contact'>Contact</h2>
-      <div className='form-contact'>
-        <input placeholder='Name' className='inputs-contact' type='text' />
-        <input placeholder='Email' className='inputs-contact' type='email' />
-        <textarea placeholder='Your Message' className='inputs-contact' type='text' />
-        <button className='btn-contact'>Send</button>
+
+      <div className='dark'>
+        <DarkVeil />
+      </div>
+      
+      <div className='container-contact'>
+
+        <div className='container-contact-info'>
+          <h2 className='contact-ways'>Contact</h2>
+          <p className='desc-contact'>To get in touch, please fill out the form.</p>
+        </div>
+
+
+        <div className='form-contact'>
+          <input placeholder='Your Name' className='inputs-contact' type='text' />
+          <input placeholder='Your Email' className='inputs-contact' type='email' />
+          <input placeholder='Your Message' className='inputs-contact' type='text' />
+          <button className='btn-contact'>Send</button>
+        </div>
+
       </div>
     </div>
   )
