ac73c4a7402c2af79ec197db851324eb6038ec41
shayan <shayanjafarnejad75@gmail.com>
2025-08-03 18:47:48 +0330

    fix the websites responsive

--- components/modules/CardProject.js
+++ components/modules/CardProject.js
@@ -2,21 +2,12 @@
 import React, { useState } from 'react'
 import cardProject from '../../styles/cardProject/cardProject.css'
 import media from '../../styles/cardProject/mediaCardProject.css'
-import Image from 'next/image'
 import FadeContent from '@/anim/FadeContent'
 
-function CardProject({ GiahLand, FitLand }) {
+function CardProject({GiahLand, FitLand}) {
 
-  const [isBlurGi, setIsBlurGi] = useState(true)
   const [isBlur, setIsBlur] = useState(true)
-
-  const blurHandlerGi = () => {
-    setIsBlurGi(false)
-  }
-
-  const UnBlurHandlerGi = () => {
-    setIsBlurGi(true)
-  }
+  const [isBlurGi, setIsBlurGi] = useState(true)
 
   const blurHandler = () => {
     setIsBlur(false)
@@ -26,22 +17,29 @@ function CardProject({ GiahLand, FitLand }) {
     setIsBlur(true)
   }
 
+  const blurHandlerGi = () => {
+    setIsBlurGi(false)
+  }
+
+  const UnBlurHandlerGi = () => {
+    setIsBlurGi(true)
+  }
+
   return (
     <>
       <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-        <div className='card-project gi' onMouseEnter={blurHandlerGi} onMouseLeave={UnBlurHandlerGi}>
-          <Image src={GiahLand?.img} width={700} height={700} alt='img-project' className={`${isBlurGi ? 'img-project' : 'img-project-unBlur'}`} />
-          <p className={`${isBlurGi ? 'desc-project-card' : 'desc-project-card-none'}`}>{GiahLand?.title} <br /> <br /> {GiahLand?.start}<br /> <br /> {GiahLand?.finish}</p>
+        <div className='card-project'>
+          <img src={GiahLand?.img} alt='img-project' className={`${isBlur ? 'img-project' : 'img-project-unBlur'}`} onMouseEnter={blurHandler} onMouseLeave={UnBlurHandler} loading='lazy' />
+          <p className={`${isBlur ? 'desc-project-card' : 'desc-project-card-none'}`} onMouseEnter={blurHandler} onMouseLeave={UnBlurHandler}>{GiahLand?.title} <br /> <br /> {GiahLand?.start}<br /> <br /> {GiahLand?.finish}</p>
         </div>
       </FadeContent>
 
       <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-        <div className='card-project' onMouseEnter={blurHandler} onMouseLeave={UnBlurHandler}>
-        <Image src={FitLand?.img} width={700} height={700} alt='img-project' className={`${isBlur ? 'img-project' : 'img-project-unBlur'}`} />
-        <p className={`${isBlur ? 'desc-project-card' : 'desc-project-card-none'}`}>{FitLand?.title} <br /> <br /> {FitLand?.start}<br /> <br /> {FitLand?.finish}</p>
+        <div className='card-project'>
+          <img src={FitLand?.img} alt='img-project' className={`${isBlurGi ? 'img-project' : 'img-project-unBlur'}`} onMouseEnter={blurHandlerGi} onMouseLeave={UnBlurHandlerGi} loading='lazy' />
+          <p className={`${isBlurGi ? 'desc-project-card' : 'desc-project-card-none'}`} onMouseEnter={blurHandlerGi} onMouseLeave={UnBlurHandlerGi}>{FitLand?.title} <br /> <br /> {FitLand?.start}<br /> <br /> {FitLand?.finish}</p>
         </div>
       </FadeContent>
-
     </>
   )
 }
--- components/templates/Projects.js
+++ components/templates/Projects.js
@@ -7,19 +7,9 @@ import SplitText from '@/anim/SplitText'
 
 function Projects() {
 
-    const GiahLand = {
-        img: '/images/giah2.jpeg',
-        title: 'GiahLand shopping site developed with React and Next',
-        start: 'Started at 2025 ⁄ 3 ⁄ 11',
-        finish: 'Finished at 2025 / 3 / 29'
-    }
+    const GiahLand = { img: '/images/giah2.jpeg', title: 'GiahLand shopping site developed with React and Next', start: 'Started at 2025 ⁄ 3 ⁄ 11', finish: 'Finished at 2025 / 3 / 29' }
 
-    const FitLand = {
-        img: '/images/fit2.png',
-        title: 'FitLand, Sport shopping site developed with React and Next',
-        start: 'Started at 2025 ⁄ 4 ⁄ 19',
-        finish: 'Finished at 2025 / 4 / 29'
-    }
+    const FitLand = { img: '/images/fit2.png', title: 'FitLand, Sport shopping site developed with React and Next', start: 'Started at 2025 ⁄ 4 ⁄ 19', finish: 'Finished at 2025 / 4 / 29' }
 
     return (
         <div className='container'>
