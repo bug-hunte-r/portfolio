10583c52fe8934163c674e686592a7c4b873c012
shayan <shayanjafarnejad75@gmail.com>
2025-08-05 22:22:08 +0330

    set modal for download the resume

--- components/templates/Header.js
+++ components/templates/Header.js
@@ -5,8 +5,12 @@ import media from '../../styles/header/mediaHeader.css'
 import TextType from '../../anim/TextType';
 import SplitText from '@/anim/SplitText';
 import FadeContent from '../../anim/FadeContent'
+import { ToastContainer, toast } from 'react-toastify';
 
 function Header() {
+
+  const notifyDownload = () => toast.success("Resume Downloaded Successfully");
+
   return (
     <div className='container'>
       <div className='conatiner-header'>
@@ -25,30 +29,41 @@ function Header() {
           textAlign="center"
         />
 
-          <div className='container-tech'>
-            <h1 className='me'>I`m</h1>
-            <TextType
-              text={["Frontend", "Backend"]}
-              typingSpeed={50}
-              pauseDuration={1800}
-              showCursor={false}
-              textColors={'#6F61C0'}
-              className='my-tech'
-              deletingSpeed={50}
-            />
-            <h1 className='me'>Developer</h1>
-          </div>
+        <div className='container-tech'>
+          <h1 className='me'>I`m</h1>
+          <TextType
+            text={["Frontend", "Backend"]}
+            typingSpeed={50}
+            pauseDuration={1800}
+            showCursor={false}
+            textColors={'#6F61C0'}
+            className='my-tech'
+            deletingSpeed={50}
+          />
+          <h1 className='me'>Developer</h1>
+        </div>
 
         <div className='container-btns-header'>
           <FadeContent blur={true} duration={1500} easing="ease-out" initialOpacity={0}>
             <button className='btn-hire'><a href={'/#contact'} className='a1'>Hire me</a></button>
           </FadeContent>
           <FadeContent blur={true} duration={1500} easing="ease-out" initialOpacity={0}>
-            <button className='btn-get-resume'><a href={'Shayan Jafarnejad.pdf'} download className='a2'>Get resume</a></button>
+            <button className='btn-get-resume'><a href={'Shayan Jafarnejad.pdf'} download className='a2' onClick={notifyDownload}>Get resume</a></button>
           </FadeContent>
         </div>
 
       </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   )
 }
