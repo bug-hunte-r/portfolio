a70e2962bf367bb0cf1602176902d985b31ce785
shayan <shayanjafarnejad75@gmail.com>
2025-08-15 11:50:45 +0330

    update skills icons

--- components/modules/CardTech.js
+++ components/modules/CardTech.js
@@ -1,35 +1,20 @@
 import React from 'react'
 import cardTech from '../../styles/cardTech/cardTech.css'
 import media from '../../styles/cardTech/mediaCardTech.css'
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { FaJsSquare } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
 import { RiReactjsLine } from "react-icons/ri";
 import { SiNextdotjs } from "react-icons/si";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiMongodb } from "react-icons/si";
 import { DiNodejs } from "react-icons/di";
 import { SiExpress } from "react-icons/si";
+import { BiLogoTypescript } from "react-icons/bi";
+import { SiNestjs } from "react-icons/si";
 import FadeContent from '@/anim/FadeContent';
 
 function CardTech() {
     return (
         <>
-
-            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-                <div className='container-cards'>
-                    <FaHtml5 className='img-card-skill html' size={70} />
-                    <h2 className='title-card-skill'>HTML</h2>
-                </div>
-            </FadeContent>
-
-            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-                <div className='container-cards'>
-                    <FaCss3Alt className='img-card-skill css' size={70} />
-                    <h2 className='title-card-skill'>CSS</h2>
-                </div>
-            </FadeContent>
-
             <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
                 <div className='container-cards'>
                     <RiTailwindCssFill className='img-card-skill tailwind' size={70} />
@@ -39,7 +24,7 @@ function CardTech() {
 
             <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
                 <div className='container-cards'>
-                    <FaJsSquare className='img-card-skill js' size={70} />
+                    <IoLogoJavascript className='img-card-skill js' size={70} />
                     <h2 className='title-card-skill'>JAVASCRIPT</h2>
                 </div>
             </FadeContent>
@@ -66,13 +51,25 @@ function CardTech() {
             <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
                 <div className='container-cards'>
                     <DiNodejs className='img-card-skill node' size={70} />
-                    <h2 className='title-card-skill'>Node</h2>
+                    <h2 className='title-card-skill'>NODE</h2>
                 </div>
             </FadeContent>
             <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
                 <div className='container-cards'>
                     <SiExpress className='img-card-skill ex' size={70} />
-                    <h2 className='title-card-skill'>Express</h2>
+                    <h2 className='title-card-skill'>EXPRESS</h2>
+                </div>
+            </FadeContent>
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <div className='container-cards'>
+                    <BiLogoTypescript className='img-card-skill type' size={70} />
+                    <h2 className='title-card-skill'>TYPESCRIPT</h2>
+                </div>
+            </FadeContent>
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <div className='container-cards'>
+                    <SiNestjs className='img-card-skill nest' size={70} />
+                    <h2 className='title-card-skill'>NEST</h2>
                 </div>
             </FadeContent>
         </>
